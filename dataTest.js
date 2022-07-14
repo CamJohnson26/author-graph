c4565d2b212fbc45b06a07b443d0356eae417bed
Cameron Johnson <cameronrossjohnson@gmail.com>
2022-07-14 09:13:28 -0500

    Checking in frontend

---
+++ dataTest.js
@@ -0,0 +1,43 @@
+var graphData = [{
+  "data": {
+    "id": "1",
+    "name": "Isaac Newton"
+  },
+  "group": "nodes",
+  "selectable": true,
+  "grabbable": true
+},{
+  "data": {
+    "id": "2",
+    "name": "Adam Smith"
+  },
+  "group": "nodes",
+  "selectable": true,
+  "grabbable": true
+},{
+  "data": {
+    "id": "3",
+    "name": "David Hume"
+  },
+  "group": "nodes",
+  "selectable": true,
+  "grabbable": true
+},{
+  "data": {
+    "source": "1",
+    "target": "2",
+    "id": "e0"
+  },
+  "group": "edges",
+  "selectable": true,
+  "grabbable": true
+},{
+  "data": {
+    "source": "3",
+    "target": "2",
+    "id": "e1"
+  },
+  "group": "edges",
+  "selectable": true,
+  "grabbable": true
+}]
