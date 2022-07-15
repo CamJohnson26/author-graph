bf649268fd5d59365c33ad5aa64126fcf6181d5f
Cameron Johnson <cameronrossjohnson@gmail.com>
2022-07-15 00:26:31 -0500

    Fixing won't load on mobile

--- code.js
+++ code.js
@@ -1,30 +1,37 @@
 
 
 var dataArray = [cyStyle, graphData];
-var cy = window.cy = cytoscape({
-  container: document.getElementById('cy'),
 
-  layout: {
-    name: 'cose',
-    idealEdgeLength: 100,
-    nodeOverlap: 20,
-    refresh: 20,
-    fit: true,
-    padding: 300,
-    randomize: false,
-    componentSpacing: 100,
-    nodeRepulsion: 100,
-    edgeElasticity: 100,
-    nestingFactor: 5,
-    gravity: 160,
-    numIter: 1000,
-    initialTemp: 10000,
-    coolingFactor: 0.95,
-    minTemp: 1.0
-  },
+document.addEventListener("DOMContentLoaded", function() {
 
-  style: dataArray[0],
+  var cy = window.cy = cytoscape({
+    container: document.getElementById('cy'),
 
-  elements: dataArray[1]
+    layout: {
+      name: 'cose',
+      idealEdgeLength: 100,
+      nodeOverlap: 20,
+      refresh: 20,
+      fit: false,
+      padding: 300,
+      randomize: false,
+      componentSpacing: 100,
+      nodeRepulsion: 100,
+      edgeElasticity: 100,
+      nestingFactor: 5,
+      gravity: 160,
+      numIter: 1000,
+      initialTemp: 10000,
+      coolingFactor: 0.95,
+      minTemp: 1.0
+    },
 
-});
+    style: dataArray[0],
+
+    elements: dataArray[1]
+
+  });
+
+  cy.center()
+  cy.fit()
+})
